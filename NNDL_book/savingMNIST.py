fa1522e63e2ea35028ad06cceee01eb03faf3b2b
nithinraok <nithinrao.koluguri@gmail.com>
2017-11-24 02:37:24 +0530

    Chapter1 end

---
+++ NNDL_book/savingMNIST.py
@@ -0,0 +1,125 @@
+
+# coding: utf-8
+
+# In[4]:
+
+
+""""
+mnist_loader
+~~~~~~~~~~~~
+
+A library to load the MNIST image data.  For details of the data
+structures that are returned, see the doc strings for ``load_data``
+and ``load_data_wrapper``.  In practice, ``load_data_wrapper`` is the
+function usually called by our neural network code.
+"""
+
+#### Libraries
+# Standard library
+import _pickle as cPickle
+import gzip
+
+# Third-party libraries
+import numpy as np
+
+def load_data():
+    """Return the MNIST data as a tuple containing the training data,
+    the validation data, and the test data.
+
+    The ``training_data`` is returned as a tuple with two entries.
+    The first entry contains the actual training images.  This is a
+    numpy ndarray with 50,000 entries.  Each entry is, in turn, a
+    numpy ndarray with 784 values, representing the 28 * 28 = 784
+    pixels in a single MNIST image.
+
+    The second entry in the ``training_data`` tuple is a numpy ndarray
+    containing 50,000 entries.  Those entries are just the digit
+    values (0...9) for the corresponding images contained in the first
+    entry of the tuple.
+
+    The ``validation_data`` and ``test_data`` are similar, except
+    each contains only 10,000 images.
+
+    This is a nice data format, but for use in neural networks it's
+    helpful to modify the format of the ``training_data`` a little.
+    That's done in the wrapper function ``load_data_wrapper()``, see
+    below.
+    """
+    f = gzip.open('mnist.pkl.gz', 'r');
+    training_data, validation_data, test_data  = cPickle.load(f,encoding='latin1')
+    #training_data, validation_data, test_data = cPickle.load(f)
+    f.close()
+    return (training_data, validation_data, test_data)
+
+def load_data_wrapper():
+    """Return a tuple containing ``(training_data, validation_data,
+    test_data)``. Based on ``load_data``, but the format is more
+    convenient for use in our implementation of neural networks.
+
+    In particular, ``training_data`` is a list containing 50,000
+    2-tuples ``(x, y)``.  ``x`` is a 784-dimensional numpy.ndarray
+    containing the input image.  ``y`` is a 10-dimensional
+    numpy.ndarray representing the unit vector corresponding to the
+    correct digit for ``x``.
+
+    ``validation_data`` and ``test_data`` are lists containing 10,000
+    2-tuples ``(x, y)``.  In each case, ``x`` is a 784-dimensional
+    numpy.ndarry containing the input image, and ``y`` is the
+    corresponding classification, i.e., the digit values (integers)
+    corresponding to ``x``.
+
+    Obviously, this means we're using slightly different formats for
+    the training data and the validation / test data.  These formats
+    turn out to be the most convenient for use in our neural network
+    code."""
+    tr_d, va_d, te_d = load_data()
+    training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
+    training_results = [vectorized_result(y) for y in tr_d[1]] #Remove Vectorized if you want number of label
+    training_data = list(zip(training_inputs, training_results))
+    validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
+    validation_data = list(zip(validation_inputs, va_d[1]))
+    test_inputs = [np.reshape(x, (784, 1)) for x in te_d[0]]
+    test_data = list(zip(test_inputs, te_d[1]))
+    return (training_data, validation_data, test_data)
+
+def vectorized_result(j):
+    """Return a 10-dimensional unit vector with a 1.0 in the jth
+    position and zeroes elsewhere.  This is used to convert a digit
+    (0...9) into a corresponding desired output from the neural
+    network."""
+    e = np.zeros((10, 1))
+    e[j] = 1.0
+    return e
+
+def saveasCSV(data,filename):
+    """Check the display image it is transposed"""
+    n=len(data)
+    dd=['label']+ ['value'+str(i) for i in np.arange(0,784)]
+    dd=np.asarray(dd)
+    a=[data[x][0] for x in np.arange(0,n)]
+    b =[data[x][1] for x in np.arange(0,n)]
+    a1=np.asarray(a)
+    a1=a1.T[0];
+    b1=np.zeros_like(a1[0])+b
+    print(a1.shape)
+    c = np.vstack((b1,a1))
+    d=np.transpose(c)
+    final=np.vstack((dd,d))
+    f=open(filename,'ab')
+    np.savetxt(f,final,fmt = "%s", delimiter=",")
+
+
+# In[5]:
+
+
+training_data, validation_data, test_data = load_data_wrapper();
+#saveasCSV(training_data,'training_data.csv');
+#saveasCSV(validation_data,'validation_data.csv');
+#saveasCSV(test_data,'test_data.csv');
+
+
+# In[3]:
+
+
+type(training_data)
+
